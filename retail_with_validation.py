582201eb0e17714eb38a50327f0b9f279b679164
petrakohva <petra.kohva@haaga-helia.fi>
2019-11-09 14:01:32 +0200

    added error catching to excersice 3

--- retail_with_validation.py
+++ retail_with_validation.py
@@ -1,9 +1,10 @@
 choise = "y"
 while choise == "y":
-    cost = float(input("Enter the item's wholesale cost: "))
-    while cost <=  0:
-        print("ERROR: the cost cannot be zero or negative")
-        cost = float(input("Enter the correct wholesale cost: "))
-    print("Retail price: $" + str(cost*2.5))
-    choise = input("Do you have another item? (Enter Y for yes): ")
-
+    try:
+        cost = float(input("Enter the item's wholesale cost: "))
+        while cost <=  0:
+            print("ERROR: the cost cannot be zero or negative")
+            cost = float(input("Enter the correct wholesale cost: "))
+        print("Retail price: $" + str(cost*2.5))
+        choise = input("Do you have another item? (Enter Y for yes): ")
+    except: print("Cost must be a number!")
